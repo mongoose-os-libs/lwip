70235bcde0b56f9c67e8da1123ef3046c5d54c36
Deomid "rojer" Ryabkov <rojer@rojer.me>
2021-03-15 21:45:06 +0000

    Fix getting DNS servers from DHCP
    Broken by a4e521993fa2bf2076a65e5ec9cc819fb55cd950

--- src/esp8266/sdk_lwip/src/core/dns.c
+++ src/esp8266/sdk_lwip/src/core/dns.c
@@ -280,7 +280,10 @@ dns_init()
 void ICACHE_FLASH_ATTR
 dns_setserver(u8_t numdns, ip_addr_t *dnsserver)
 {
-  if ((numdns < DNS_MAX_SERVERS) && (dns_pcb != NULL) &&
+  if ((numdns < DNS_MAX_SERVERS) &&
+#if !LWIP_DNS_NO_CLIENT
+      (dns_pcb != NULL) &&
+#endif
       (dnsserver != NULL) && !ip_addr_isany(dnsserver)) {
     dns_servers[numdns] = (*dnsserver);
   }

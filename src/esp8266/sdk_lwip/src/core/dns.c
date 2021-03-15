mongoose-os-libs/lwip

commit a4e521993fa2bf2076a65e5ec9cc819fb55cd950

esp8266: Disable LWIP DNS resolver
We don't use it anyway

src/esp8266/sdk_lwip/src/core/dns.c
@@ -244,6 +244,7 @@ dns_init()
 
   LWIP_DEBUGF(DNS_DEBUG, ("dns_init: initializing\n"));
 
+#if !LWIP_DNS_NO_CLIENT
   /* if dns client not yet initialized... */
   if (dns_pcb == NULL) {
     dns_pcb = udp_new();
@@ -265,6 +266,9 @@ dns_init()
 #if DNS_LOCAL_HOSTLIST
   dns_init_local();
 #endif
+#else
+  (void) dns_recv;
+#endif // !LWIP_DNS_NO_CLIENT
 }
 
 /**

mos.yml
@@ -59,6 +59,7 @@ conds:
               TCP_MSS: 1460
               TCP_WND: 2920
               MEMP_NUM_TCP_PCB: 20
+              LWIP_DNS_NO_CLIENT: 1
 
   # TODO(rojer): When mos supports more elaborate conds, this can be deduped.
   - when: mos.platform == "stm32"

src/esp8266/sdk_lwip/include/lwip/dns.h
@@ -107,8 +107,10 @@ void           dns_init(void);
 void           dns_tmr(void);
 void           dns_setserver(u8_t numdns, ip_addr_t *dnsserver);
 ip_addr_t      dns_getserver(u8_t numdns);
+#if !LWIP_DNS_NO_CLIENT
 err_t          dns_gethostbyname(const char *hostname, ip_addr_t *addr,
                                  dns_found_callback found, void *callback_arg);
+#endif
 
 #if DNS_LOCAL_HOSTLIST && DNS_LOCAL_HOSTLIST_IS_DYNAMIC
 int            dns_local_removehost(const char *hostname, const ip_addr_t *addr);

src/esp8266/sdk_lwip/include/lwipopts.h
@@ -802,6 +802,10 @@
 #define LWIP_DNS                        1
 #endif
 
+#ifndef LWIP_DNS_NO_CLIENT
+#define LWIP_DNS_NO_CLIENT              0
+#endif
+
 /** DNS maximum number of entries to maintain locally. */
 #ifndef DNS_TABLE_SIZE
 #define DNS_TABLE_SIZE                  4

src/esp8266/sdk_lwip/src/core/sntp.c
@@ -53,7 +53,7 @@
 #include "lwip/pbuf.h"
 #include "lwip/app/time.h"
 //#include <string.h>
-#if LWIP_UDP
+#if LWIP_UDP && !LWIP_DNS_NO_CLIENT
 
 /**
  * SNTP_DEBUG: Enable debugging for SNTP.

src/esp8266/sdk_lwip/src/core/timers.c
@@ -222,7 +222,7 @@ igmp_timer(void *arg)
 }
 #endif /* LWIP_IGMP */
 
-#if LWIP_DNS
+#if LWIP_DNS && !LWIP_DNS_NO_CLIENT
 /**
  * Timer callback function that calls dns_tmr() and reschedules itself.
  *
@@ -257,7 +257,7 @@ void sys_timeouts_init(void)
 #if LWIP_IGMP
   sys_timeout(IGMP_TMR_INTERVAL, igmp_timer, NULL);
 #endif /* LWIP_IGMP */
-#if LWIP_DNS
+#if LWIP_DNS && !LWIP_DNS_NO_CLIENT
   sys_timeout(DNS_TMR_INTERVAL, dns_timer, NULL);
 #endif /* LWIP_DNS */
 

commit 70235bcde0b56f9c67e8da1123ef3046c5d54c36

Fix getting DNS servers from DHCP
Broken by a4e521993fa2bf2076a65e5ec9cc819fb55cd950

src/esp8266/sdk_lwip/src/core/dns.c
@@ -280,7 +280,10 @@ dns_init()
 void ICACHE_FLASH_ATTR
 dns_setserver(u8_t numdns, ip_addr_t *dnsserver)
 {
-  if ((numdns < DNS_MAX_SERVERS) && (dns_pcb != NULL) &&
+  if ((numdns < DNS_MAX_SERVERS) &&
+#if !LWIP_DNS_NO_CLIENT
+      (dns_pcb != NULL) &&
+#endif
       (dnsserver != NULL) && !ip_addr_isany(dnsserver)) {
     dns_servers[numdns] = (*dnsserver);
   }